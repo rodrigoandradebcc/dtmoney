2d5bd80d0eb20e78f0014d74980ad516b6a519bc
Rodrigo Andrade <randrade@brbatel.com.br>
2021-03-20 16:58:36 -0300

    Movendo criação para o context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
-import Modal from 'react-modal'
-
 import { Container, Content } from './styles';
+
 
 interface HeaderProps {
   onOpenNewTransactionModal: () => void
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 
@@ -13,18 +14,23 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-  const [type, setType] = useState('deposit');
-  const [title, setTitle] = useState('')
-  const [value, setValue] = useState(0)
+  const { createTransaction } = useContext(TransactionsContext)
+
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState('')
+  const [type, setType] = useState('deposit')
 
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = { title, value, category, type }
-
-    api.post('/transactions', data);
+    createTransaction({
+      title,
+      amount,
+      category, 
+      type,
+    })
   }
 
   return (
@@ -47,8 +53,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         />
 
 
-        <input placeholder="Valor" type="number" value={value}
-          onChange={event => setValue(Number(event.target.value))} />
+        <input placeholder="Valor" type="number" value={amount}
+          onChange={event => setAmount(Number(event.target.value))} />
 
         <TransactionTypeContainer>
           <RadioBox
